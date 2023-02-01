c0eaf5cf51f5ac4afc18b8817e1a20005ae7fcef
clark <csstudent@LI052-00390L.pugetsound.edu>
2023-02-01 00:16:50 -0800

    Making progress towards remaking the notes I lost in xcode crash

--- Swift Practice.playground/Pages/Basic Operators.xcplaygroundpage/Contents.swift
+++ Swift Practice.playground/Pages/Basic Operators.xcplaygroundpage/Contents.swift
@@ -1,2 +1,46 @@
+    // Constants
+// constants cannot change variables can
+// constants are preferred
+// variables and constants can be named with any symbols including emojis
 
-// Constants
+let constant = 6
+
+var variable = 5
+variable += 7
+
+    // Types
+// Type declaration
+// Types can be set using a colon : which represents of the type
+
+let explicitType: Int = 50
+
+// multiple variables can be declared at once
+// Explicitly typed variables do not need to have an initial value
+let mulitCon1, multiCon2, multiCon3: Double
+
+// Variable/constant names can be swift key woods in necessary using back ticks however this is not preferred
+let `let` = "let"
+
+
+
+// Integers
+
+
+
+// Doubles/Floats
+
+
+// Booleans
+
+
+
+
+// Randomness
+
+
+// Printing
+// Use print to print out values
+// to include inline values in print use backslash and brackets \()
+let inline = "inline"
+print("We are talking about \(inline) printing right now")
+
